65ccc9885be7a4a0f0663198c555fb4b9341a9a8
Szymon Miśtal <mistalzaq@gmail.com>
2024-11-28 14:27:01 +0100

    Fix rust template by using comments instead of docs

--- src/templates/rust/src/main.rs
+++ src/templates/rust/src/main.rs
@@ -1,18 +1,16 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     println!("Part One: {}", part_one(&input));
-    /**scaf**println!("Part Two: {}", part_two(&input));**scaf**/
+    //**scaf**|println!("Part Two: {}", part_two(&input));
 }
 
 fn part_one(input: &str) -> i32 {
     -1
 }
 
-/**scaf**
-fn part_two(input: &str) -> i32 {
-    -1
-}
-**scaf**/
+//**scaf**|fn part_two(input: &str) -> i32 {
+//**scaf**|    -1
+//**scaf**|}
 
 #[cfg(test)]
 mod tests {
@@ -24,11 +22,9 @@ mod tests {
         assert_eq!(part_one(input), {{ANSWER_1}});
     }
 
-    /**scaf**
-    #[test]
-    fn part_two_example() {
-        let input = "{{INPUT_2}}";
-        assert_eq!(part_two(input), {{ANSWER_2}});
-    }
-    **scaf**/
+    //**scaf**|#[test]
+    //**scaf**|fn part_two_example() {
+    //**scaf**|    let input = "{{INPUT_2}}";
+    //**scaf**|    assert_eq!(part_two(input), {{ANSWER_2}});
+    //**scaf**|}
 }
