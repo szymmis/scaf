7b48d1e359b64d84d7e77c3d5b539cb0e3811d25
Szymon Miśtal <mistalzaq@gmail.com>
2024-11-29 21:56:48 +0100

    Fix python template

--- templates/python/test_main.py
+++ templates/python/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 
-from main import part_one #**scaf**|, part_two
+from main import part_one  #**scaf**|, part_two
+
 
 class TestParts(unittest.TestCase):
     def test_part_one(self):
@@ -9,7 +10,8 @@ class TestParts(unittest.TestCase):
 
     #**scaf**|def test_part_two(self):
     #**scaf**|   input = """{{INPUT_2}}"""
-    #**scaf**|   self.assertEqual(part_one(input), {{ANSWER_2}})
+    #**scaf**|   self.assertEqual(part_two(input), {{ANSWER_2}})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
